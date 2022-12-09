73776c25db24249ef1c47bf7de5278e57c352781
Hanif <87808600+finahdinner@users.noreply.github.com>
2022-12-09 13:06:54 +0000

    Logging added as a class definited in a separate .py file - successfully works in logging both on the console (for debugging) and to the log file (for errors etc)

--- Project/extract_emote.py
+++ Project/extract_emote.py
@@ -1,11 +1,12 @@
-import requests
 from PIL import Image, ImageSequence
-from bs4 import BeautifulSoup
-import logging
+import requests
 
 """ Logging configuration """
-
-# logging.basicConfig(level=logging.DEBUG)
+from for_logging import MyLogger
+from pathlib import Path
+file_name = Path(__file__).stem # get name of the current file (without the .py)
+# Usage: my_logger.logger.[LEVEL]('[LOG MESSAGE]')
+my_logger = MyLogger(file_name=file_name, log_file_path=f"logs/{file_name}.log") # create an instance of MyLogger
 
 
 """ File paths """
@@ -19,24 +20,26 @@ CONVERTED_EMOTES_PATH = "converted_emotes/"
 def get_emote_id(page_url: str) -> str:
     """ Extract 7TV emote ID from the page_url given """
     emote_id = page_url.split("/")[-1]
+    my_logger.logger.debug(f'{emote_id=}')
     return emote_id
 
 
 def get_emote_url(emote_id: str) -> str:
     """ Extract the exact emote path from the emote ID given """
     emote_url = f"https://cdn.7tv.app/emote/{emote_id}/4x.webp"
+    my_logger.logger.debug(f'{emote_url=}')
     return emote_url
 
 
 def download_emote(emote_id: str, emote_url: str) -> str:
     """ Download the (webp) emote with the emote url given, and return its path """
-
     response = requests.get(emote_url)
     if response.status_code != 200:
         return ""
     downloaded_img_path = f"{DOWNLOADED_EMOTES_PATH}{emote_id}.webp"
     with open(downloaded_img_path, "wb") as img:
         img.write(response.content) # response.content is the image content
+    my_logger.logger.debug(f'SUCCESS - Downloaded Image. Path = {downloaded_img_path}.')
     return downloaded_img_path
 
 
@@ -55,16 +58,22 @@ def is_animated(img_path: str) -> bool:
 
 def convert_img(emote_id:str, file_path: str) -> str:
     """ Convert the webp image into a gif, in order to be uploaded to Discord, and return its path. """
-    img = Image.open(file_path)
-    img.info.pop('background', None) # remove background
+    try: # big try-except block, I know this bad practice!
+        img = Image.open(file_path)
+        img.info.pop('background', None) # remove background
 
-    if is_animated(file_path): # check if it's animated
-        img_extension = "gif"
-    else:
-        img_extension = "png"
+        if is_animated(file_path): # check if it's animated
+            img_extension = "gif"
+        else:
+            img_extension = "png"
 
-    converted_img_path = f"{CONVERTED_EMOTES_PATH}{emote_id}.{img_extension}"
-    img.save(converted_img_path, save_all=True)
+        converted_img_path = f"{CONVERTED_EMOTES_PATH}{emote_id}.{img_extension}"
+        img.save(converted_img_path, save_all=True)
+        my_logger.logger.debug(f'SUCCESS - Converted Image. Path = {converted_img_path}.')
+
+    except:
+        return ""
+    
     return converted_img_path
 
 
@@ -72,13 +81,16 @@ def main(page_url: str) -> str | bool:
     """ Main function. Downloads the image from the given url and converts to either
     a gif (if animated) or a png format (if not animated), returning the file path. """
 
-    try:
-        emote_id = get_emote_id(page_url)
-        emote_url = get_emote_url(emote_id)
-        downloaded_img_path = download_emote(emote_id, emote_url)
-        converted_img_path = convert_img(emote_id, downloaded_img_path)
-    except:
-        return False # if unsuccessful
+    emote_id = get_emote_id(page_url)
+    emote_url = get_emote_url(emote_id)
+    downloaded_img_path = download_emote(emote_id, emote_url)
+    if not downloaded_img_path: # if it failed to find the url
+        my_logger.logger.error(f'Emote URL for --{page_url}-- not found.')
+        return False
+    converted_img_path = convert_img(emote_id, downloaded_img_path)
+    if not converted_img_path: # if it failed to load the downloaded file
+        my_logger.logger.error(f'Failed to convert image for --{page_url}--.')
+        return False
 
     return converted_img_path # return file path if the process was successful
 
@@ -89,5 +101,16 @@ if __name__ == "__main__":
     # downloaded_img_path = f"{download_emote(emote_id, emote_url)=}"
     # print(downloaded_img_path)
 
-    page_url = "https://7tv.app/emotes/60af769d2c36aae19e32ec9d"
+    page_url = "https://7tv.app/emotes/60abf171870d317bef23d399"
     main(page_url)
+
+    # convert_img('yo', 'hello.png')
+    # import sys    
+    # import os    
+    # file_name =  os.path.basename(sys.argv[0])
+    # print(file_name)
+
+
+    #  from pathlib import Path
+    #  file_name = Path(__file__).stem
+    #  print(file_name)
---
+++ Project/for_logging.py
@@ -0,0 +1,46 @@
+import logging
+
+# MyLogger class, which can be instantiated for each file that needs logging abilities.
+class MyLogger:
+    """ Usage: MyLogger.logger.[LEVEL]('[LOG MESSAGE]') """
+
+    def __init__(self, file_name, log_file_path):
+        self.file_name = file_name
+        self.log_file_path = log_file_path # log file path
+
+        # logger
+        self.logger = logging.getLogger(self.file_name)
+        self.logger.setLevel(logging.DEBUG) # unless a handler is specified otherwise, will log DEBUG and above
+
+        # logger handler for the log file
+        self.file_handler = logging.FileHandler(self.log_file_path)
+        self.file_handler.setLevel(logging.ERROR) # only log to file for ERRORS and above
+        self.file_formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s') # format of log lines
+        self.file_handler.setFormatter(self.file_formatter)
+        self.logger.addHandler(self.file_handler)
+
+        # logger handler for the to console - will log all things (debug and above)
+        self.stream_handler = logging.StreamHandler()
+        self.stream_formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+        self.stream_handler.setFormatter(self.stream_formatter)
+        self.logger.addHandler( self.stream_handler)
+
+
+
+    # # logger
+    # logger = logging.getLogger(__name__)
+    # logger.setLevel(logging.DEBUG) # unless a handler is specified otherwise, will log DEBUG and above
+
+    # # logger handler for the log file
+    # log_file_path = 'logs/extract_emote.log' # log file path
+    # file_handler = logging.FileHandler(log_file_path)
+    # file_handler.setLevel(logging.ERROR) # only log to file for ERRORS and above
+    # file_formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s') # format of log lines
+    # file_handler.setFormatter(file_formatter)
+    # logger.addHandler(file_handler)
+
+    # # logger handler for the to console - will log all things (debug and above)
+    # stream_handler = logging.StreamHandler()
+    # stream_formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+    # stream_handler.setFormatter(stream_formatter)
+    # logger.addHandler(stream_handler)
